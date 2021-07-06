b69930577a70ad259c950a45dbd82ec4099ec005
KOUASSI CHARLES OTH-NIEL <kouassicharlesothniel@gmail.com>
2021-07-06 10:30:51 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let nb = ''
+// // do
+// nb = Number(prompt('Entrer un nombre'))
+// console.log(typeof(nb))
+// // while (nb > 1 || nb < 3)
+// // alert('valider')
+nb = parseInt(prompt('Entrer un nombre'))
+if(nb == ''){
+    console.log('vide ')
+}else{
+ console.log('rempli')
+}
+
